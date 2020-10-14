9fb50296116e0f0d366a20d5df0952f587f5c391
unknown <joseph1999@naver.com>
2020-10-14 20:36:39 +0900

    modify eventdecorator

--- calendar_fragment/java/com/example/myapplication/EventDecorator.java
+++ calendar_fragment/java/com/example/myapplication/EventDecorator.java
@@ -5,17 +5,18 @@ import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 
 public class EventDecorator implements DayViewDecorator {
 
     private final int color;
-    private final HashSet<CalendarDay> dates;
+    public HashSet<CalendarDay> dates;
 
-    public EventDecorator(int color, Collection<CalendarDay> dates) {
+    public EventDecorator(int color, HashSet<CalendarDay> dates) {
         this.color = color;
-        this.dates = new HashSet<>(dates);
+        this.dates = dates;
     }
 
     @Override
