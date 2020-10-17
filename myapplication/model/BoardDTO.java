bb61637a262068d8090a9923409bd533f66e8c63
0nandon <71716834+0nandon@users.noreply.github.com>
2020-10-17 22:29:43 +0900

    Add files via upload

---
+++ myapplication/model/BoardDTO.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.model;
+
+public class BoardDTO {
+    public String manager="";
+    public String name="";
+    public String explain="";
+    public long timestamp=0;
+}
