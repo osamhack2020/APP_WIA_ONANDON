da27d8ea4dcdcb2a302f854b82af305543ef8a58
0nandon <71716834+0nandon@users.noreply.github.com>
2020-10-19 22:29:44 +0900

    Add files via upload

--- myapplication/PostMore.java
+++ myapplication/PostMore.java
@@ -19,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.myapplication.Club.ClubPostItemMore;
+import com.example.myapplication.MyGoalPost.MyGoalPostIngMore;
 import com.example.myapplication.MyGoalPost.ScrollMygoalMore;
 import com.example.myapplication.model.CommentDTO;
 import com.example.myapplication.model.MyGoalContentDTO;
@@ -30,8 +31,13 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Transaction;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class PostMore extends AppCompatActivity {
@@ -40,6 +46,7 @@ public class PostMore extends AppCompatActivity {
 
     FirebaseFirestore firestore;
     FirebaseAuth auth;
+    FirebaseStorage storage;
 
     Toolbar toolbar;
     ImageView backPressed;
@@ -61,6 +68,7 @@ public class PostMore extends AppCompatActivity {
 
         firestore = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
 
         imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
@@ -155,12 +163,82 @@ public class PostMore extends AppCompatActivity {
         int id = item.getItemId();
         //분기한다.
         switch(id){
-            case R.id.update : return true;
+            case R.id.update :
+                final DocumentReference docRef = firestore.collection(documentUid).document(postUid);
+                firestore.runTransaction(new Transaction.Function<Void>() {
+
+                    @Nullable
+                    @Override
+                    public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                        DocumentSnapshot snapshot = transaction.get(docRef);
+                        PostDTO postDTO = snapshot.toObject(PostDTO.class);
+
+                        Intent updateIntent = new Intent(PostMore.this, UpdatePost.class);
+                        updateIntent.putExtra("documentUid", documentUid);
+                        updateIntent.putExtra("postUid", postUid);
+                        if(postDTO.isPhoto == 1){
+                            updateIntent.putExtra("imageUri", postDTO.imageUri);
+                        }
+                        startActivityForResult(updateIntent, FINISH);
+                        return null;
+                    }
+                });
+                return true;
             case R.id.delete :
-                Intent intent = new Intent(this, DeletePostAsk.class);
-                startActivityForResult(intent, FINISH);
+                Intent deleteIntent = new Intent(this, DeletePostAsk.class);
+                startActivityForResult(deleteIntent, FINISH);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == FINISH){
+            if(resultCode == RESULT_OK) {
+
+                final DocumentReference docRef = firestore.collection(documentUid).document(postUid);
+                firestore.runTransaction(new Transaction.Function<Void>() {
+                    StorageReference httpsReference = storage.getReference();
+
+                    @Nullable
+                    @Override
+                    public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                        DocumentSnapshot snapshot = transaction.get(docRef);
+                        PostDTO postDTO = snapshot.toObject(PostDTO.class);
+
+                        if(postDTO.isPhoto == 1){
+                            httpsReference = storage.getReferenceFromUrl(postDTO.imageUri);
+                            httpsReference.delete();
+                        }
+                        return null;
+                    }
+                });
+
+                firestore.collection(documentUid).document(postUid).delete();
+                firestore.collection(postUid).get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            ArrayList<String> contentUidList = new ArrayList<>();
+
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for (QueryDocumentSnapshot value : queryDocumentSnapshots) {
+                                    contentUidList.add(value.getId());
+                                }
+                                for (int i = 0; i < contentUidList.size(); i++) {
+                                    firestore.collection(postUid).document(contentUidList.get(i)).delete();
+                                }
+                            }
+                        });
+
+                Toast.makeText(PostMore.this, "게시물이 삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else if(resultCode == RESULT_FIRST_USER){
+                finish();
+            }
+        }
     }
 }
