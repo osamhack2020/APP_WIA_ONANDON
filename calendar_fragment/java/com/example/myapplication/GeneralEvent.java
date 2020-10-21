a037ec5b8662b9bde97fbd6110ab229edda0da27
unknown <joseph1999@naver.com>
2020-10-21 20:15:38 +0900

    editting comments on generalevent.java

--- calendar_fragment/java/com/example/myapplication/GeneralEvent.java
+++ calendar_fragment/java/com/example/myapplication/GeneralEvent.java
@@ -32,6 +32,8 @@ public class GeneralEvent {
         this.name = name;
         this.dates = new HashSet<>(dates);
 
+        // 일정의 날짜를 넘겨주어 EventDecorator 생성
+        // 색깔은 임의로 지정
         color = ((int)(Math.random()*16777215)) | (0xFF << 24);
         decorator = new EventDecorator(color, this.dates);
     }
